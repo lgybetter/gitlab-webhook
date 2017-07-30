ce82c4a3b5ab004221a40ee0f2ed0fccbe1ff9e4
lgybetter <437675103@qq.com>
2017-07-30 21:32:41 +0800

    fixed the roomId bug

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 const config = require('./config')
 
-const getRoomId = (projectName) => {
+const getRoomId = projectName => {
   let projects = config.projects
   for(let project of projects) {
     if(project.name === projectName) {
